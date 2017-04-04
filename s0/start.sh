9b0f46acf9ef2b124ec87990f8b57d1cfec36b78
Aaron Addleman <aaron.addleman@autodesk.com>
2017-04-04 16:44:45 -0700

    updates

--- s0/start.sh
+++ s0/start.sh
@@ -3,7 +3,7 @@ docker run --name=consul0 \
        -p 8500:8500 -p 8600:8600/udp \
        -v "$PWD/data":/consul/data \
        -v "$PWD/config":/consul/config \
-       docker-consul \
+       consul \
        consul agent \
        -ui \
        -client 0.0.0.0 \
