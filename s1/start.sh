a7b08e9b253606fd9f7d01dc0563be5ec3dcecc6
Aaron Addleman <aaronaddleman@gmail.com>
2017-04-04 16:44:45 -0700

    updates

--- s1/start.sh
+++ s1/start.sh
@@ -2,7 +2,7 @@ docker run --name=consul1 \
        -d \
        -v "$PWD/data":/consul/data \
        -v "$PWD/config":/consul/config \
-       docker-consul \
+       consul \
        consul agent \
        -join 172.17.0.2 \
        -config-file=/consul/config/config.json
